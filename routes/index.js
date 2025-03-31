dc3830568f13a0abd5b8f83effe7f27c86555a58
katiii-git <katymartagon@gmail.com>
2025-03-31 12:13:53 -0600

    Subiendo restapi al repositorio

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const { Router } =require('express');
+const controllers =require('../controllers');
+
+const router= Router();
+
+router.get('/', (req, res) => res.send('Welcome'));
+
+router.post('/users', controllers.createUser);
+
+router.get('/users', controllers.getAllUsers);
+
+module.exports = router;
