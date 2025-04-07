e6c000427ae15f07ef138522104bfba4fdce3ee7
katiii-git <katymartagon@gmail.com>
2025-04-07 11:58:25 -0600

    Cambio de base de datos .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const server=require('./server');
-
+//h
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
