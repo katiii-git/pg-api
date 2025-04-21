5aa79c5928baac1e337fe67f60b6f2ec60502e89
katiii-git <katymartagon@gmail.com>
2025-04-21 11:30:26 -0600

    añadiendo update y delete

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,51 @@ const createUser = async (req, res)=> {
 	}
 };
 
+const  deleteUser  = async (req,  res)  =>  {
+   console.log('deleting user...');
+
+   try {
+      const user = await models.User.findOne({ where: { id: req.params.id } });
+      if (user) {
+	  console.log(user);
+          await user.destroy();
+      }
+      else {
+         return res.status(200).json( { "error ": req.params.id  +  " no existe"});
+      }
+      return res.status(200).json( { "deleted ": req.params.id });
+   }
+   catch  (error) {
+      return res.status(500).send ( { error: error.message  } );
+   }
+
+};
+
+const updateUser  = async (req,  res)  =>  {
+   console.log('updating user...');
+
+   try {
+      const user = await models.User.findOne({ where: { id: req.params.id } });
+      if (user) {
+	  console.log(user);
+          user.name = req.body.name;
+          user.email = req.body.email;
+          user.age = req.body.age;
+          user.comments = req.body.comments;
+          await user.save();
+      }
+      else {
+         return res.status(200).json( { "error ": req.params.id  +  " no existe"});
+      }
+
+      return res.status(200).json( { "updated ": user });
+   }
+   catch  (error) {
+      return res.status(500).send ( { error: error.message  } );
+   }
+
+};
+
 const getAllUsers = async (req, res) => {
   console.log('getting users');
   try{
@@ -26,5 +71,7 @@ const getAllUsers = async (req, res) => {
 
 module.exports = {
   createUser,
-  getAllUsers
+  getAllUsers,
+  deleteUser,
+  updateUser
 };
--- database/config/config.js
+++ database/config/config.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
-
+const fs=require("fs");
 module.exports={
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: "postgres",
     
   },
   test: {
      url: process.env.TEST_DATABASE_URL,
-     dialect: 'postgres',
+     dialect: "postgres",
   },
   production: {
      url: process.env.DATABASE_URL,
-     dialect: 'postgres',
+     dialect: "postgres",
   },
 }
